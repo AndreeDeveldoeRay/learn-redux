0a409a548afa422dc4b6ee766c3ee4c8c171d2fd
Andrée Ray <info@andreeray.se>
2017-03-05 15:15:00 +0100

    add location

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,21 +4,13 @@
 * @Email:  me@andreeray.se
 * @Filename: redux-example.jsx
 * @Last modified by:   DevelDoe
-* @Last modified time: 2017-03-04T03:44:10+01:00
+* @Last modified time: 2017-03-05T14:48:44+01:00
 */
 
 
 
-console.log('starting redux example')
-var redux = require('redux')
-var stateDefault =
-{
-    name:'Anonymous',
-    hobbies: [],
-    movies: []
-}
-var nextHobbyId = 0,
-    nextMovieId = 0
+var redux = require('redux'), axios = require('axios')
+
 var nameReducer = (state = 'Anonymous', action) =>
 {
     switch (action.type)
@@ -29,6 +21,15 @@ var nameReducer = (state = 'Anonymous', action) =>
             return state
     }
 }
+var changeName = (name) =>
+{
+    return {
+        type: 'CHANGE_NAME',
+        name
+    }
+}
+
+var nextHobbyId = 0
 var hobbiesReducer = (state = [], action) =>
 {
     switch (action.type)
@@ -47,6 +48,21 @@ var hobbiesReducer = (state = [], action) =>
             return state
     }
 }
+var addHobbie = (hobby) =>
+{
+    return {
+        type: 'ADD_HOBBY',
+        hobby
+    }
+}
+var removeHobbie = (id) =>
+{
+    return {
+        type: 'REMOVE_HOBBY',
+        id
+    }
+}
+var nextMovieId = 0
 var moviesReducer = (state = [], action) =>
 {
     switch (action.type)
@@ -66,65 +82,80 @@ var moviesReducer = (state = [], action) =>
             return state
     }
 }
+var addMovie = (title,genre) =>
+{
+    return {
+        type: 'ADD_MOVIE',
+        title,
+        genre
+    }
+}
+var removeMovie = (id) =>
+{
+    return {
+        type: 'REMOVE_MOVIE',
+        id
+    }
+}
+var mapReducer = (state = {isFetching: false, url: undefined}, action) =>
+{
+    switch (action.type)
+    {
+        case 'START_LOCATION_FETCH':
+            return {
+                isFetching: true,
+                url: undefined
+            }
+        case 'COMPLETE_LOCATION_FETCH':
+            return {
+                isFetching: false,
+                url: action.url
+            }
+        default:
+            return state
+    }
+}
+var startLocationFetch = () => { return { type: 'START_LOCATION_FETCH' } }
+var completeLocationFetch = (url) =>
+{
+    return {
+        type: 'COMPLETE_LOCATION_FETCH',
+        url
+    }
+}
+var fetchLocation = () =>
+{
+    store.dispatch(startLocationFetch())
+    axios.get('http://ipinfo.io').then(function (res)
+    {
+        var loc = res.data.loc
+        var baseUrl = 'http://maps.google.com?q='
+        store.dispatch(completeLocationFetch(baseUrl+loc))
+    })
+}
 var reducer = redux.combineReducers(
 {
     name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map : mapReducer
 })
 var store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? window.devToolsExtension() : f => f ))
-var unsubscribe = store.subscribe(() =>
-{
+var unsubscribe = store.subscribe(() => {
     var state = store.getState()
-    document.getElementById('app').innerHTML = state
-    console.log('New State',store.getState())
-})
+    if (state.map.isFetching) document.getElementById('app').innerHTML = "loading..."
+    else if (state.map.url) document.getElementById('app').innerHTML = "<a target=_blank href='" + state.map.url + "'>View your location</a>"
 
+})
 var currentState = store.getState()
 
-console.log('currentState', currentState)
+fetchLocation()
 
-
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Andree'
-})
-
-store.dispatch(
-{
-    type: 'ADD_HOBBY',
-    hobby: 'Running'
-})
-store.dispatch(
-{
-    type: 'ADD_HOBBY',
-    hobby: 'Walking'
-})
-store.dispatch(
-{
-    type: 'REMOVE_HOBBY',
-    id: 1
-})
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Ilona'
-})
-
-store.dispatch(
-{
-    type: 'ADD_MOVIE',
-    title: 'Star Wars',
-    genre: 'Sci-fi'
-})
-store.dispatch(
-{
-    type: 'ADD_MOVIE',
-    title: 'Mad Max',
-    genre: 'Action'
-})
-store.dispatch(
-{
-    type: 'REMOVE_MOVIE',
-    id: 1
-})
-// unsubscribe()
+store.dispatch(changeName('ray'))
+store.dispatch(changeName('Ilona'))
+store.dispatch(addHobbie('Electronics'))
+store.dispatch(addHobbie('Development'))
+store.dispatch(removeHobbie(1))
+store.dispatch(addMovie('Star Wars','Sci-Fi'))
+store.dispatch(addMovie('Mad Max','Action'))
+store.dispatch(removeMovie(1))
